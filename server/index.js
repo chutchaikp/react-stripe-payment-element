3ec540ddfe8663204afc8f661d70b73149634e9b
chutchai.kp <chutchai.kp@gmail.com>
2023-03-20 08:47:00 +0700

    commit

--- server/index.js
+++ server/index.js
@@ -7,28 +7,22 @@ const port = 5000
 const dotenv = require('dotenv')
 dotenv.config();
 
-// var bodyParser = require('body-parser');
 app.use(express.json());
 app.use(cors())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.post('/create-payment-intent', async (req, res) => {
-	// console.info('body', req.body)
-	// res.json(req.body);
-	// debugger;
 	const skey = process.env.STRIPE_SECRET_KEY
 	const stripe = new Stripe(skey)
-
 	try {
 		const paymentIntent = await stripe.paymentIntents.create({
-			currency: 'eur',
+			currency: 'thb',
 			amount: 3099,
 			automatic_payment_methods: {
 				enabled: true,
 			}
 		})
-
 		res.send({ clientSecret: paymentIntent.client_secret })
 	} catch (error) {
 		console.log(error)
